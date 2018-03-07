83fc9acaeaa4a26bf42f0701125eb29260cdeb46
Miles Budden <miles@budden.net>
2018-03-07 23:01:53 +0000

    The vectors now correctly generate

--- DocSearch.py
+++ DocSearch.py
@@ -1,14 +1,14 @@
 """
 Student No.: C1769331
 """
-
+import math
 
 def generateDictionary(docs):
     dictionary = []
     for doc in docs:
         for word in doc[0].split():
             dictionary.append(word)
-    dictionary = set(dictionary)
+    dictionary = list(set(dictionary))
     return dictionary
 
 
@@ -23,17 +23,32 @@ def generateInvertedIndex(dictionary, corpus):
     return index
 
 
+def calculateAngle(doc1, doc2, dictionary):
+    # Split each document down into individual words
+    doc1, doc2 = doc1.split(), doc2.split()
+    # Create empty vectors
+    doc1_vector = [0]*len(dictionary)
+    doc2_vector = [0]*len(dictionary)
+    for i, word in enumerate(dictionary):
+        doc1_vector[i] = doc1.count(word)
+    for i, word in enumerate(dictionary):
+        doc2_vector[i] = doc2.count(word)
+    return
+
+
+
+
 def main():
     # Placeholders for data to be imported/generated
     docs = []
     queries = []
 
     # Load the corpus
-    with open("./corpus/set2/docs.txt") as fp:
+    with open("./corpus/set1/docs.txt") as fp:
         docs = [(x.strip('\n'), i) for i, x in enumerate(fp.readlines(), 1)]
 
     # Load the queries
-    with open("./corpus/set2/queries.txt") as fp:
+    with open("./corpus/set1/queries.txt") as fp:
         queries = [(x.strip('\n'), i) for i, x in enumerate(fp.readlines(), 1)]
 
     dictionary = generateDictionary(docs)
@@ -52,8 +67,10 @@ def main():
                 word_documents.append(set(invertedIndex[word]))
         # Calculate the intersection
         related = set.intersection(*word_documents)
-        print("Related:", " ".join(str(x) for x in related))
-
+        print("Relevant documents:", " ".join(str(x) for x in related))
+        for document in docs:
+            if document[1] in related:
+                calculateAngle(document[0], query[0], dictionary)
 
 if __name__ == "__main__":
     main()
